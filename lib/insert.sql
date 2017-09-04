313883c8ee132fa110b11a2ee401ca3868ec5918
stopvoldemort <david.squires1@gmail.com>
2017-09-04 16:15:05 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,65 @@
+
+
+
+INSERT INTO projects VALUES (1, "one", "charitable", 1000, "september 1", "october 1");
+INSERT INTO projects VALUES (2, "two", "charitable", 2000, "september 1", "october 1");
+INSERT INTO projects VALUES (3, "three", "music", 3000, "september 1", "october 1");
+INSERT INTO projects VALUES (4, "four", "charitable", 4000, "september 1", "october 1");
+INSERT INTO projects VALUES (5, "five", "charitable", 5000, "september 1", "october 1");
+INSERT INTO projects VALUES (6, "six", "music", 6000, "september 1", "october 1");
+INSERT INTO projects VALUES (7, "seven", "charitable", 7000, "september 1", "october 1");
+INSERT INTO projects VALUES (8, "eight", "charitable", 8000, "september 1", "october 1");
+INSERT INTO projects VALUES (9, "nine", "charitable", 9000, "september 1", "october 1");
+INSERT INTO projects VALUES (10, "ten", "charitable", 10000, "september 1", "october 1");
+
+INSERT INTO users VALUES (1, "david", 18);
+INSERT INTO users VALUES (2, "phil", 24);
+INSERT INTO users VALUES (3, "steve", 29);
+INSERT INTO users VALUES (4, "scott", 45);
+INSERT INTO users VALUES (5, "george", 42);
+INSERT INTO users VALUES (6, "alvin", 17);
+INSERT INTO users VALUES (7, "sylvester", 26);
+INSERT INTO users VALUES (8, "bob", 22);
+INSERT INTO users VALUES (9, "jake", 31);
+INSERT INTO users VALUES (10, "mitch", 53);
+INSERT INTO users VALUES (11, "claire", 23);
+INSERT INTO users VALUES (12, "allison", 76);
+INSERT INTO users VALUES (13, "sela", 12);
+INSERT INTO users VALUES (14, "phyllipa", 26);
+INSERT INTO users VALUES (15, "marissa", 45);
+INSERT INTO users VALUES (16, "erin", 33);
+INSERT INTO users VALUES (17, "dani", 24);
+INSERT INTO users VALUES (18, "jenna", 32);
+INSERT INTO users VALUES (19, "kate", 55);
+INSERT INTO users VALUES (20, "emily", 39);
+
+INSERT INTO pledges VALUES (1, 15, 14, 8);
+INSERT INTO pledges VALUES (2, 140, 19, 5);
+INSERT INTO pledges VALUES (3, 200, 3, 4);
+INSERT INTO pledges VALUES (4, 20, 15, 1);
+INSERT INTO pledges VALUES (5, 100, 15, 2);
+INSERT INTO pledges VALUES (6, 100, 17, 3);
+INSERT INTO pledges VALUES (7, 100, 18, 7);
+INSERT INTO pledges VALUES (8, 100, 14, 3);
+INSERT INTO pledges VALUES (9, 100, 11, 8);
+INSERT INTO pledges VALUES (10, 100, 19, 3);
+INSERT INTO pledges VALUES (11, 100, 14, 10);
+INSERT INTO pledges VALUES (12, 50, 2, 4);
+INSERT INTO pledges VALUES (13, 50, 4, 6);
+INSERT INTO pledges VALUES (14, 50, 8, 2);
+INSERT INTO pledges VALUES (15, 50, 3, 8);
+INSERT INTO pledges VALUES (16, 50, 7, 5);
+INSERT INTO pledges VALUES (17, 50, 1, 2);
+INSERT INTO pledges VALUES (18, 50, 2, 9);
+INSERT INTO pledges VALUES (19, 50, 5, 6);
+INSERT INTO pledges VALUES (20, 50, 7, 8);
+INSERT INTO pledges VALUES (21, 50, 8, 5);
+INSERT INTO pledges VALUES (22, 50, 10, 3);
+INSERT INTO pledges VALUES (23, 50, 10, 2);
+INSERT INTO pledges VALUES (24, 50, 15, 6);
+INSERT INTO pledges VALUES (25, 25, 17, 8);
+INSERT INTO pledges VALUES (26, 25, 18, 3);
+INSERT INTO pledges VALUES (27, 25, 14, 2);
+INSERT INTO pledges VALUES (28, 67, 19, 7);
+INSERT INTO pledges VALUES (29, 67, 20, 9);
+INSERT INTO pledges VALUES (30, 75, 1, 2);
